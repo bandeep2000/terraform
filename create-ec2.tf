1c76f1953b342333fc6f10296fbbdc3aad9152aa
bandeep2000 <bandeep2000@gmail.com>
2018-10-16 21:03:09 -0700

    test

--- create-ec2.tf
+++ create-ec2.tf
@@ -2,15 +2,30 @@ provider "aws" {
  region = "us-east-1"
 }
 
+variable "tag" {
+  type    = "string"
+  default = "machine"
+}
+
 variable "key" {
   type    = "string"
-  default = "value"
+  default = "ban1-1-2"
+}
+
+variable "ami" {
+  type    = "string"
+  default = "ami-04681a1dbd79675a5"
+}
+
+variable "itype" {
+  type    = "string"
+  default = "t2.micro"
 }
 resource "aws_instance" "example" {
- ami = "ami-04681a1dbd79675a5"
- instance_type = "t2.micro"
- key_name = "ban1-1-2"
+ ami = "${var.ami}"
+ instance_type = "${var.itype}"
+ key_name = "${var.key}"
  tags {
-  Name = "${var.key}"
+  Name = "${var.tag}"
  }
 }
